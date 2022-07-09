4de2ce38de91f05b723d98766d0aa70c0217d31b
hershrana <hershrana@gmail.com>
2022-07-10 00:24:33 +0530

    cm

---
+++ jk/src/test/java/jk/JkApplicationTests.java
@@ -0,0 +1,13 @@
+package jk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
