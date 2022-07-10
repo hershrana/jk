ba5988f141856b04a7461146ae378d1084bf1212
hershrana <hershrana@gmail.com>
2022-07-10 17:37:20 +0530

    docker file

--- jk/src/main/java/jk/JkApplication.java
+++ jk/src/main/java/jk/JkApplication.java
@@ -1,14 +1,25 @@
 package jk;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JkApplication.class, args);
 		System.out.println("op");
+	}
+	@GetMapping(path = "/welcome")
+	public String welcome()
+	{
+		return "Welcome !!,"+LocalDateTime.now();
 	}
 
 }
